3abdd47379570a9c41badce29ce1f19308f613d8
Patrick Speranza <patrick.m.speranza@gmail.com>
2022-12-14 20:24:30 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+words = [
+  "MOM",
+  "DAD",
+  "ELLIE",
+  "LUCY",
+  "BENNY",
+  "I",
+  "ME",
+  "YOU",
+  "DOG",
+  "CAT",
+  "GO",
+  "IT",
+  "ON",
+  "WE",
+]
+
+function getWord() {
+  let randomNum = Math.floor(Math.random() * words.length)
+  const h2 = document.querySelector("h2")
+  let oldWord = h2.innerText
+  let newWord = words[randomNum]
+  oldWord === newWord ? getWord() : (h2.innerText = newWord)
+}
